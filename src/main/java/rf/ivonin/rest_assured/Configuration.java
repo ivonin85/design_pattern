6334d3bf260a4101d174deeedb4a4da31c9a432e
Sergey Ivonin <ivonin85@gmail.com>
2022-05-03 15:52:23 +0300

    modified COnfiguration

--- src/main/java/rf/ivonin/rest_assured/Configuration.java
+++ src/main/java/rf/ivonin/rest_assured/Configuration.java
@@ -1,14 +1,15 @@
 package rf.ivonin.rest_assured;
 
+import io.restassured.RestAssured;
 import org.testng.annotations.BeforeSuite;
 
 public class Configuration {
 
     @BeforeSuite(alwaysRun = true)
     public void beforeSuite() {
-        /**
-         RestAssured.basePath = "basePath";
-         RestAssured.requestSpecification = requestSpecification();
-         **/
+
+        RestAssured.basePath = "basePath";
+        RestAssured.requestSpecification = Specification.requestSpecification();
+
     }
 }
