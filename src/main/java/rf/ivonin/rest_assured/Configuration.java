cb4630a4521a857cbd6aba7d8772d81132aca1ee
Sergey Ivonin <ivonin85@gmail.com>
2022-05-03 15:50:33 +0300

    dt

--- src/main/java/rf/ivonin/rest_assured/Configuration.java
+++ src/main/java/rf/ivonin/rest_assured/Configuration.java
@@ -1,20 +1,14 @@
 package rf.ivonin.rest_assured;
 
-import io.restassured.RestAssured;
 import org.testng.annotations.BeforeSuite;
-
-import static rf.ivonin.rest_assured.Specification.requestSpecification;
 
 public class Configuration {
 
     @BeforeSuite(alwaysRun = true)
     public void beforeSuite() {
-
-        //RestAssured.basePath = "basePath";
-
-        // можно задать одну спецификацию для всех запросов:
-        RestAssured.requestSpecification = requestSpecification();
-
-
+        /**
+         RestAssured.basePath = "basePath";
+         RestAssured.requestSpecification = requestSpecification();
+         **/
     }
 }
